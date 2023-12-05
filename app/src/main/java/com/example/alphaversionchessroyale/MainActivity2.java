fcd0e75f8ab29962ca23ef2af14238f90036b153
Idannnn <idan.bunkin@gmail.com>
2023-12-05 10:04:37 +0200

    fixec permission in the storage

--- app/src/main/java/com/example/alphaversionchessroyale/MainActivity2.java
+++ app/src/main/java/com/example/alphaversionchessroyale/MainActivity2.java
@@ -3,9 +3,11 @@ package com.example.alphaversionchessroyale;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -31,12 +33,16 @@ import java.util.UUID;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_PERMISSION = 3;
     private final int GALLERY_REQ_CODE = 1000;
     ImageView imgGallery;
     StorageReference storageRef;
     Uri image;
     String name;
     StorageReference storageReference;
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,40 +52,46 @@ public class MainActivity2 extends AppCompatActivity {
         Button btnGallery = findViewById(R.id.galleryButton);
         FirebaseApp.initializeApp(MainActivity2.this);
         storageReference = FirebaseStorage.getInstance().getReference();
-
-
-        btnGallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent iGallery = new Intent(Intent.ACTION_PICK);
-                iGallery.setType("image/*");
-                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(iGallery, GALLERY_REQ_CODE);
-            }
-        });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
-    {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission to access external storage granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Permission to access external storage NOT granted", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK){
-            if(requestCode==GALLERY_REQ_CODE){
-                name =UUID.randomUUID().toString();
-                StorageReference refrence = storageReference.child("images/" + name);
-                refrence.putFile(data.getData()).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Toast.makeText(MainActivity2.this, "Uploaded!", Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity2.this, "Uplaod Failed!", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-
+        if (resultCode == RESULT_OK) {
+            if (requestCode == GALLERY_REQ_CODE) {
+                if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted, proceed with the upload
+                    name = UUID.randomUUID().toString();
+                    StorageReference reference = storageReference.child("images/" + name);
+                    reference.putFile(data.getData()).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(MainActivity2.this, "Uploaded!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity2.this, "Upload Failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    // Permission not granted
+                    Toast.makeText(MainActivity2.this, "Permission to access external storage not granted", Toast.LENGTH_LONG).show();
+                }
             }
         }
     }
@@ -126,5 +138,20 @@ public class MainActivity2 extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void upload(View view) {
+        // Check permission before initiating the upload
+        requestPermission();
+        if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            // Permission granted, proceed with image selection
+            Intent iGallery = new Intent(Intent.ACTION_PICK);
+            iGallery.setType("image/*");
+            iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(iGallery, GALLERY_REQ_CODE);
+        } else {
+            // Permission not granted, do nothing or show a message
+            Toast.makeText(this, "Permission to access external storage is required for image upload", Toast.LENGTH_LONG).show();
+        }
     }
 }
